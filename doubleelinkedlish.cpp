c95413a82d3851f59a22db6e03ca86f51fcb8645
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-04-28 15:19:26 +0700

    step4

--- doubleelinkedlish.cpp
+++ doubleelinkedlish.cpp
@@ -57,4 +57,18 @@ public:
             return;
         }
 
+        // insert in between node
+        // Step 8: Locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
+
        
