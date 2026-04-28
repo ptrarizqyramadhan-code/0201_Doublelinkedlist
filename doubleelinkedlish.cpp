7e608ee833437fbda4e27ab4e648101c5dcd993e
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-04-28 15:17:39 +0700

    step 2

--- doubleelinkedlish.cpp
+++ doubleelinkedlish.cpp
@@ -10,3 +10,28 @@ public:
     Node *next;
     Node *prev;
 };
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+
+        // Step 1: Allocate memory for new node
+        Node *newNode = new Node();
+
+        // Step 2: Assign value to the data fields
+        newNode->noMhs = nim;
+
+        
